13b5f2f108a3b13ab207309e2cac81fa9940cf37
MShahryiar <muhammadshahryiar@yahoo.com>
2022-06-18 11:46:56 +0500

    First Commit

---
+++ src/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './HomeScreen.css';
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        <h1>Let's build Netflix</h1>
+        <div>
+            <h2>Navbar</h2>
+        </div>
+        <div>
+            <h2>Banner</h2>
+        </div>
+        <div>
+            <h2>Rows</h2>
+        </div>
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJGo8do9_JL30dyuOfFBpAGlFu1erA3ts",
+    authDomain: "netflix-build-72c1f.firebaseapp.com",
+    projectId: "netflix-build-72c1f",
+    storageBucket: "netflix-build-72c1f.appspot.com",
+    messagingSenderId: "999074720611",
+    appId: "1:999074720611:web:04b0e2a4b52808f69a4156"
+  };
